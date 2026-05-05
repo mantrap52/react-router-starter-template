a95262149f5d1e9531a69b5c54a515550e5d2659
Mantra Patel <mantra@Mac.gateway>
2026-05-06 00:41:39 +1000

    Restored Dakshin Global files

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  // This tells the app: "At the main URL, show the home.tsx file"
+  index("./routes/home.tsx"),
+] satisfies RouteConfig;
